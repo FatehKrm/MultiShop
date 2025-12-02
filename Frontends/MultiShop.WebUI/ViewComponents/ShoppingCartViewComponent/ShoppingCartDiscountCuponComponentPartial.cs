8fcadf1e83362e7354e900b212470f375a2083f2
FatehKrm <fatehkarampour@outlook.com>
2025-12-02 22:35:16 +0300

    Frontend 2.12.2025

---
+++ Frontends/MultiShop.WebUI/ViewComponents/ShoppingCartViewComponent/ShoppingCartDiscountCuponComponentPartial.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MultiShop.WebUI.ViewComponents.ShoppingCartViewComponent
+{
+    public class ShoppingCartDiscountCuponComponentPartial : ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            return View();
+        }
+    }
+}
