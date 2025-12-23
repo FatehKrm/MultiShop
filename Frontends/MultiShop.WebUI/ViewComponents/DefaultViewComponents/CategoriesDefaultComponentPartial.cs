3fceab226727870168bd87490ba9bae411714565
FatehKrm <fatehkarampour@outlook.com>
2025-12-23 13:19:19 +0300

    ...

--- Frontends/MultiShop.WebUI/ViewComponents/DefaultViewComponents/CategoriesDefaultComponentPartial.cs
+++ Frontends/MultiShop.WebUI/ViewComponents/DefaultViewComponents/CategoriesDefaultComponentPartial.cs
@@ -1,11 +1,28 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using MultiShop.DtoLayer.CatalogDtos.CategoryDtos;
+using Newtonsoft.Json;
 
 namespace MultiShop.WebUI.ViewComponents.DefaultViewComponents
 {
     public class CategoriesDefaultComponentPartial : ViewComponent
     {
-        public IViewComponentResult Invoke()
+        private readonly IHttpClientFactory _httpClientFactory;
+
+        public CategoriesDefaultComponentPartial(IHttpClientFactory httpClientFactory)
         {
+            _httpClientFactory = httpClientFactory;
+        }
+
+        public async Task <IViewComponentResult> InvokeAsync()
+        {
+            var client = _httpClientFactory.CreateClient();
+            var responseMessage = await client.GetAsync("https://localhost:7070/api/Categories");  // Yapacağım isteğin türünü belirleyecek ve adresini
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                var jasonData = await responseMessage.Content.ReadAsStringAsync(); // gelen veriyi string formatında okuyacak
+                var values = JsonConvert.DeserializeObject<List<ResultCategoryDto>>(jasonData); // gelen string veriyi de deserialize edip CategoryDto tipine dönüştürecek
+                return View(values);
+            }
             return View();
         }
     }
